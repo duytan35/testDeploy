0a278afc5de86bb52705ef33c5227b301d9489c8
20120574 <duytan030522@gmail.com>
2023-11-16 10:09:13 +0700

    aaaa

--- src/routes/index.js
+++
@@ -1,9 +0,0 @@
-const authRouter = require("./auth");
-const userRouter = require("./users");
-
-const route = (app) => {
-  app.use("/auth", authRouter);
-  app.use("/users", userRouter);
-};
-
-module.exports = route;
